4262bb282e2b5a47b1b6df806bf292efa338b73e
Pratham Gupta <guptapratham661@gmail.com>
2025-06-19 16:02:54 +0530

    Enhance CORS configuration to specify allowed origins for production and local development environments.

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+const corsOptions = {
+    origin:['https://jobportal-beta-eight.vercel.app','http://localhost:5173'],
+    credentials:true
+}
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
